b731744a6c3e6042d52af39d3d5175a4807def20
Clement Nkamanyi <cnkamanyi@hotmail.com>
2024-03-25 23:41:30 +0100

    added terraform

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "resource_group_name" {
+  default = "poc-rg"
+}
+
+variable "state_resource_group_name" {
+  default = "poc-rg"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+# Web app
+variable "funcapp_name" {
+  default = "poc-webapp"
+}
+
+variable "storage_account_name" {
+  default = "pocstorageaccount"
+}
+
+variable "state_storage_account_name" {
+  default = "pocstorageaccount"
+}
+variable "state_container_name" {
+  default = "container"
+}
+variable "state_key" {
+  default = "key"
+}
