7c3b34d8af89c3bf50f1dd384a7230985966815c
Clement Nkamanyi <cnkamanyi@hotmail.com>
2024-03-26 00:06:15 +0100

    formatted terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,23 +1,23 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "~>2.0"
     }
   }
   backend "azurerm" {
-        resource_group_name  = ""
-        storage_account_name = ""
-        container_name       = ""
-        key                  = ""
-    }
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
+  }
 }
 
 provider "azurerm" {
-  subscription_id   = "<azure_subscription_id>"
-  tenant_id         = "<azure_subscription_tenant_id>"
-  client_id         = "<service_principal_appid>"
-  client_secret     = "<service_principal_password>"
-  
+  subscription_id = "<azure_subscription_id>"
+  tenant_id       = "<azure_subscription_tenant_id>"
+  client_id       = "<service_principal_appid>"
+  client_secret   = "<service_principal_password>"
+
   features {}
 }
