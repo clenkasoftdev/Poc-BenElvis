01a1a718ba55053a5e853f046475824aa0e09599
Clement Nkamanyi <cnkamanyi@hotmail.com>
2024-03-25 17:29:11 +0100

    Added function app, an initial test and its CI[C

---
+++ src/FunctionApp/Clenka.BenElvisPoc.FuncApp/Clenka.BenElvisPoc.FuncApp.SaveRsvp/SaveRsvp.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Clenka.BenElvisPoc.FuncApp.SaveRsvp
+{
+    public class SaveRsvp
+    {
+        private readonly ILogger<SaveRsvp> _logger;
+
+        public SaveRsvp(ILogger<SaveRsvp> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("SaveRsvp")]
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
